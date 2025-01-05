df4901b3687ea6df44efc0d090a3d229c6690baf
Dynee <lucaspadden@gmail.com>
2025-01-05 17:18:55 -0500

    day03 part 1 completion.

--- 2024/day03/main.cpp
+++ 2024/day03/main.cpp
@@ -39,41 +39,80 @@ bool is_valid(const char& c, const char& last) {
     return false;
 }
 
+int product_of_mul(const std::string& mul) {
+    std::cout << "mul: " << mul << "\n";
+    int product = 0;
+    std::string leftStr = "";
+    std::string rightStr = "";
+    bool seen_separator = false;
+
+    for (int i = 0; i < mul.size(); i++) {
+        if (isdigit(mul[i]) && seen_separator) {
+            std::cout << "right: " << mul[i] << "\n";
+            rightStr += mul[i];
+        } else if (mul[i] == ',') {
+            std::cout << "seen separator\n";
+            seen_separator = true;
+        }
+        else if (isdigit(mul[i])) {
+            std::cout << "left: " << mul[i] << "\n";
+            leftStr += mul[i];
+        }
+    }
+
+    std::cout << "left: " << leftStr << " right: " << rightStr << "\n";
+    if (rightStr == "") {
+        return 0;
+    }
+    product = std::stoi(leftStr) * std::stoi(rightStr);
+    return product;
+}
+
 int main() {
     std::cout << "Enter a file: ";
     std::string file_name;
     std::cin >> file_name;
 
     std::ifstream file(file_name);
-    std::string input;
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string input = buffer.str();
 
-    while (std::getline(file, input)) {
-        std::ostringstream oss;
+    std::ofstream out("output.txt");
 
-        std::vector<std::string> muls;
+    std::cout << "input size: " << input.size() << "\n";
+    std::ostringstream oss;
 
-        char last = 'm';
+    std::vector<std::string> muls;
 
-        for (int i = 0; i < input.size(); i++) {
-            if (is_valid(input[i], last)) {
-                oss << input[i];
-                last = input[i];
+    char last = 'm';
 
-                if (input[i] == ')') {
-                    muls.push_back(oss.str());
-                    oss.str("");
-                    last = 'm';
-                }
-            } else {
+    for (auto i = 0; i < input.size(); i++) {
+        if (is_valid(input[i], last)) {
+            oss << input[i];
+            last = input[i];
+
+            if (input[i] == ')') {
+                muls.push_back(oss.str());
                 oss.str("");
+                oss.clear();
                 last = 'm';
             }
-        }
-
-        for (auto &mul : muls) {
-            std::cout << "mul: " << mul << "\n";
+        } else {
+            oss.str("");
+            oss.clear();
+            last = 'm';
         }
     }
+
+    long sum = 0;
+
+    for (auto &mul : muls) {
+        out << mul << "\n";
+        sum += product_of_mul(mul);
+    }
+
+    std::cout << "answer: " << sum << "\n";
 
     return 0;
 }
